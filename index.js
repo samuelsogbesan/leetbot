8bf3e1b5ab8f4d4ff0280e557a98fcdf421c882a
samsog <juwonsogbesan@gmail.com>
2021-02-06 16:50:53 +0000

    Detect when user leaves/joins designated queue ⛷️

--- index.js
+++ index.js
@@ -15,4 +15,21 @@ client.on('message', msg => {
   }
 });
 
+client.on('voiceStateUpdate', (oldChannel, newChannel) => {
+  /**
+   * The ID of the queue object.
+   */
+  const queueID = 807286526408130590;
+
+  if (oldChannel.channelID == queueID) {
+    // they left channel
+    const {id, username} = oldChannel.member.user;
+    console.log(`user ${username} (${id}) left queue.`);
+  } else if (newChannel.channelID == queueID) {
+    // they joined channel
+    const {id, username} = newChannel.member.user;
+    console.log(`user ${username} (${id}) joined queue.`);
+  }
+})
+
 client.login(BOT_TOKEN);
