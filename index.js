82ac0e203e43110ca48e7121914d33c34ff3b606
samsog <juwonsogbesan@gmail.com>
2021-02-10 14:11:55 +0000

    Use appropriate comparison operator (===)

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ client.on('voiceStateUpdate', (oldChannel, newChannel) => {
    */
   const queueID = 807286526408130590;
 
-  if (oldChannel.channelID == queueID) {
+  if (oldChannel.channelID === queueID) {
     // User left the queue channel.
     const serverId = oldChannel.member.guild.id;
     const {id, username} = oldChannel.member.user;
 
     removePlayerFromQueue(serverId, id);
     console.log(`user ${username} (${id}) left queue.`);
-  } else if (newChannel.channelID == queueID) {
+  } else if (newChannel.channelID === queueID) {
     // User joined the queue channel.
     const serverId = newChannel.member.guild.id;
     const {id, username} = newChannel.member.user;
