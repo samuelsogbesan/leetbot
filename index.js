5dfbc14d5c5d9384208c544170037bfb7dc649b4
samsog <juwonsogbesan@gmail.com>
2021-02-10 14:11:29 +0000

    nit: fix comments & add readability spaces

--- index.js
+++ index.js
@@ -22,15 +22,17 @@ client.on('voiceStateUpdate', (oldChannel, newChannel) => {
   const queueID = 807286526408130590;
 
   if (oldChannel.channelID == queueID) {
-    // they left channel
+    // User left the queue channel.
     const serverId = oldChannel.member.guild.id;
     const {id, username} = oldChannel.member.user;
+
     removePlayerFromQueue(serverId, id);
     console.log(`user ${username} (${id}) left queue.`);
   } else if (newChannel.channelID == queueID) {
-    // they joined channel
+    // User joined the queue channel.
     const serverId = newChannel.member.guild.id;
     const {id, username} = newChannel.member.user;
+
     offerPlayerToQueue(serverId, id);
     console.log(`user ${username} (${id}) joined queue.`);
   }
