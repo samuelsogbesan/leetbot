b2a37240018abc5070fb6f3c0304413fcdb8414f
samsog <juwonsogbesan@gmail.com>
2021-02-12 21:24:12 +0000

    Remove async/await from removePlayerFromQueue

--- queries/Queue.js
+++ queries/Queue.js
@@ -41,7 +41,7 @@ const offerPlayerToQueue = async (serverId, user) =>
  * @param {*} serverId 
  * @param {*} user 
  */
-const removePlayerFromQueue = async (serverId, user) => await dispose(QueueTable.child(serverId).child(user));
+const removePlayerFromQueue = (serverId, user) => dispose(QueueTable.child(serverId).child(user));
 
 /**
  * Returns the number of players in the queue
