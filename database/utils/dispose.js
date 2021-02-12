957b56ce46937b8e54354c1d0ca65c9acd2ed53e
samsog <juwonsogbesan@gmail.com>
2021-02-12 20:51:07 +0000

    nit: add comment 🧨

--- database/utils/dispose.js
+++ database/utils/dispose.js
@@ -1,3 +1,9 @@
+/**
+ * Safely deletes a Firebase database Reference after use.
+ * Useful for deleting data when it is no longer needed.
+ * @param {*} Reference a firebase reference.
+ * @throws if there is an error removing the Reference.
+ */
 const dispose = async (Reference) => {
   try {
     await Reference.remove();
