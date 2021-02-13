b2c6cacae91b38729ff317fde5e6cbe4b577df93
samsog <juwonsogbesan@gmail.com>
2021-02-13 02:38:22 +0000

    nit: refine grammar comment

--- commands/grammar.js
+++ commands/grammar.js
@@ -1,5 +1,7 @@
 /**
- * A series of regular expressions denoting the valid set of tokens.
+ * A series of regular expressions denoting the valid set of symbols.
+ * The symbols are ordered in order of precedence. Earlier ranked symbols
+ * are matched first.
  */
 const grammar = [
   '-[a-zA-z]+[a-zA-z0-9]*',
