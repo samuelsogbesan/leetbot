7bd48191645edfaa6707b16598484808b80ffa86
samsog <juwonsogbesan@gmail.com>
2021-02-13 17:04:40 +0000

    Document Executer command

---
+++ commands/executer.js
@@ -0,0 +1,10 @@
+/**
+ * Responsible for executing commands.
+ * @param {*} command - an object following the command schema.
+ * A command is an object with a "command" string and a series of argument/value pairs.
+ * @returns true if the command was executed correctly
+ * @throws if the command could not be executed.
+ */
+const executer = (command) => {
+
+}
