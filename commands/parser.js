983d57432dc468d93841c9cb0053b8e1bbb2a49d
samsog <juwonsogbesan@gmail.com>
2021-02-13 02:04:17 +0000

    nit: update comment to reflect command schema

--- commands/parser.js
+++ commands/parser.js
@@ -3,7 +3,8 @@
  * @param {*} text - the input text to be parsed for a command.
  * Expects text to be in the form '-commandName --arg1...argn, -commandName2 ...'
  * @returns the resulting command(s) as an array of objects,
- * mapping from { commandName : [arguments] }.
+ * mapping from { commandName: { arg1: [val1], arg2: [val1, val2], } }.
+ * An argument's value will be undefined if it is a flag.
  * @throws if the input text is malformed.
  */
 const parse = (text) => {
