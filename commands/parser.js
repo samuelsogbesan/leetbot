b3c191665bf44018c76be560b575611dc6b7aad6
samsog <juwonsogbesan@gmail.com>
2021-02-13 15:50:09 +0000

    Consume commands with parser() 🥝

--- commands/parser.js
+++ commands/parser.js
@@ -1,18 +1,45 @@
-const grammar = require('./grammar');
-
 /**
- * Entry point to the parsing mechanism.
+ * Consumes a command string and returns it, split up into the command and its arguments.
  * @param {*} text - the input text to be parsed for a command.
  * Expects text to be in the form '-commandName --arg1...argn, -commandName2 ...'
  * @returns the resulting command(s) as an array of objects,
  * mapping from { commandName: { arg1: [val1], arg2: [val1, val2], } }.
- * An argument's value will be undefined if it is a flag.
+ * An argument's value will be null if it is a flag.
  * @throws if the input text is malformed.
  */
-const parse = (grammar, text) => {
+const parse = (text) => {
+  //const commandExpr = /^(-[a-zA-Z]+[\w]*[^\s])+(\s-{2}[a-zA-z]+[\w]*(\s\w+)*)*$/;
+  //^(-[a-zA-Z]+[\w]*[^\s])+((\s-{2}[a-zA-z]+[\w]*(?>\s\w+[^\s-])*)*)$
+  /**
+   * The regular expression for a command of the form --command --arg1 optionalva1 optionval2...
+   */
+  const commandExpr = /^(-\w+){1}(\s--\w+(\s\w+)*)*$/;
 
+  if(commandExpr.test(text)) {
+    const command = /^(-\w+)/g.exec(text)[0];
+    const rawArgs = text.match(/(\s--\w+(\s\w+)*)/g);
+
+    return {
+      command: command,
+      args: rawArgs.map(arg => {
+        var values = /\s\w+/g.exec(arg);
+        if (values) {
+          // Grab all the values as an array.
+          values = arg.match(/\s\w+/g);
+
+          // Do some post processing on the values to remove lingering spaces
+          for (var i = 0; i < values.length; i++) values[i] = values[i].trim();
+        }
+
+        return ({
+          arg: /--\w+[^\s]/g.exec(arg)[0],
+          values: values
+        });
+      })
+    }
+  } else {
+    return new Error('Can\'t parse input');
+  }
 }
-
-parse(grammar, '-commandName --arg1 --arg2 value1 value2 value3 --arg3 value1');
 
 module.exports = parse;
