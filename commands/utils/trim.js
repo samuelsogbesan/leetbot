43c097f72227325a22910a95e6a6509ed16e5a1d
samsog <juwonsogbesan@gmail.com>
2021-02-13 18:20:27 +0000

    Add param to trimArg

--- commands/utils/trim.js
+++ commands/utils/trim.js
@@ -16,11 +16,11 @@ const trimCommand = (command) => {
  * @param {*} command 
  * @throws if the given error is illformed.
  */
-const trimArg = () => {
-  if (!/^-\w+$/.test(command)) {
-    return new Error('Command is does not conform to regex.');
+const trimArg = (arg) => {
+  if (!/^-\w+$/.test(arg)) {
+    return new Error('Argument does not conform to regex.');
   } else {
-    return command.substring(2, command.length);
+    return arg.substring(2, arg.length);
   }
 }
 
