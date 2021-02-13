63f3e484acce1d3d49056d634196c81b5d37b2e1
samsog <juwonsogbesan@gmail.com>
2021-02-13 21:55:18 +0000

    Update example command

--- commands/provider/CommandProvider.js
+++ commands/provider/CommandProvider.js
@@ -4,7 +4,7 @@ const CommandProvider = {};
 const commands = {};
 
 // Define each command here.
-commands.session = new Command((args) => console.log(`hi with args ${args}`));
+commands.helloWorld = new Command((args) => console.log(`Hello World! arguements: ${args.arg1}`));
 
 // The entry point to access the commands.
 CommandProvider.getCommand = (commandName) => {
