4f12204d041d66ad7f13b44eb06335e37b1b72ef
samsog <juwonsogbesan@gmail.com>
2021-02-16 23:03:11 +0000

    Import and run bot 🏃🏾‍♀️

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-const Discord = require('discord.js');
+const bot = require('./bot');
 require('dotenv').config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.channel.send('pong');
-  }
-});
-
-client.on('voiceStateUpdate', (oldChannel, newChannel) => {
-  /**
-   * The ID of the channel being used as the queue.
-   */
-  const queueID = 807286526408130590;
-
-  if (oldChannel.channelID == queueID) {
-    // they left channel
-    const {id, username} = oldChannel.member.user;
-    console.log(`user ${username} (${id}) left queue.`);
-  } else if (newChannel.channelID == queueID) {
-    // they joined channel
-    const {id, username} = newChannel.member.user;
-    console.log(`user ${username} (${id}) joined queue.`);
-  }
-})
-
-client.login(BOT_TOKEN);
+bot.login(BOT_TOKEN);
