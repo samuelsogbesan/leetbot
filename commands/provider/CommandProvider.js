e74f6fd33fcdedd8360fb79249e33d0c589aa18a
samsog <juwonsogbesan@gmail.com>
2021-05-13 12:51:51 +0100

    Use Map<String, Command> over object for commands

--- commands/provider/CommandProvider.js
+++ commands/provider/CommandProvider.js
@@ -3,17 +3,16 @@ const InvalidCommandError = require("../errors/InvalidCommandError");
 const { newQuestion } = require("../exports/newQuestion");
 
 const CommandProvider = {};
-const commands = {};
+const commands = new Map();
 
-// Define each command here.
-commands.helloWorld = new Command((args) => console.log(`Hello World! arguements: ${args.arg1}`));
-
-commands.new = newQuestion;
+// Define new commands here.
+commands.set('new', newQuestion);
 
 // The entry point to access the commands.
 CommandProvider.getCommand = (commandName) => {
-  if (commands[commandName]) {
-    return commands[commandName];
+  let command = commands.get(commandName);
+  if (command) {
+    return command;
   } else {
     throw new InvalidCommandError(`${commandName} is not a command.`);
   }
